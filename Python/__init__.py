14490b3c22e05f1e00402dc4c170a9eb47378245
AD <aaronjdodd1989@gmail.com>
2022-06-03 17:39:48 +0300

    KISS

--- Python/__init__.py
+++ Python/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask , render_template
 
-def create_app():
-    app = Flask(__name__) 
-#mostly for ensuring this step isn't forgotten, not useful for this web - I believe this is backend work though.    
-    app.config['SECRET_KEY'] = '1234'
+app = Flask(__name__) 
 
-    return app
+
+@app.route("/") 
+def index():
+    return render_template("index.html")
+
+   
