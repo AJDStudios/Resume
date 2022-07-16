3185240bd62d5c6eb7501576d4ed3d8722d091e6
AD <aaronjdodd1989@gmail.com>
2022-07-16 14:14:47 +0300

    local root correct

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import{ Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar () {
     return (
 	 <nav className="nav">
-        <Link to="/">
+        <Link to="/" className="site-title">
 			AD's Resume
 		</Link>
             <ul className="nav-links">
