d7c0a2a0141c9ebe5a69cc22b71e601618f9e57f
AJDstudios <aaronjdodd1989@gmail.com>
2023-05-23 21:17:13 +0100

    content changes in skills and about

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -36,21 +36,25 @@ export default function Skills() {
                     <ul>
                         <h4>Code and script skills</h4>
                         <li> Git/Github </li>
-                        <li> HTML/CSS </li>
-                        <li> Javascript </li>
-                        <li> Python - Mostly in the context of game dev - but I can also use Flask </li>
-                        <li> Java - experience in the context of game plugins </li>
+                        <li> HTML/CSS/Javascript </li>
+                        <li> React.js incl. vite </li>
+                        <li> Python</li>
+                        <li> node.js </li>
+                        <li> next.js </li>
+                        <li> Worked with various others as part of a stack </li>
+                        <li> Not afraid of tackling something I've never seen </li>
                     </ul> 
                 </section>
                 <section className="swskills">
                     <div className="codesw">
                         <ul>
                             <h4>Coding Software</h4>
-                            <li> Visual Studio Code </li>
-                            <li> Notepad ++ </li>
-                            <li> Atom </li>
-                            <li> IntelliJ </li>
+                            <li> Sanity.io and sanity studio</li>
+                            <li> Visual Studio Code, Notepad ++, Atom, and dabbled with others. </li>
                             <li> Ren'py </li>
+                            <li> chatGPT probably counts at this stage </li>
+                            <li> Adept with google </li>
+                            <li> Docker </li>
                         </ul>
                     </div>
                     <div>
@@ -77,19 +81,20 @@ export default function Skills() {
                         <li> Wrting/Editing </li>
                         <li> Proofreading (English) and Translation </li>
                         <li> Communication (including deescalation in a hostile setting) </li>
+                        <li> Cross-department communication </li>
                     </ul>
                 </section>
                 <section className="work">
                     <h4>Work History</h4>
-                    <p>Though I've worked in many fields, they are not directly relevant to software devlopment. I can provide a detailed work history if requested.</p>
+                    <p>Though I've worked in many fields, they are not directly relevant to software devlopment. I can provide a detailed work history if requested - but here's a highlights reel.</p>
                         <ul>
+                            <li>Frontend Software Engineer</li>
                             <li>IP - Private English tutor</li>
                             <li>Eight years experience as an employed EFl Teacher - UK, Czech Republic, Russia</li>
                             <li>Guitar Tutor - Freelance</li>
                             <li>Freelance Editor, writer, translator</li>
                             <li>Casino Card Room Supervisor</li>
                             <li>Prison Service</li>
-                            <li>Early years in Retail as worker and middle management.</li>
                         </ul>
                 </section>
                 <section className="education">
@@ -98,9 +103,7 @@ export default function Skills() {
                         <li>TEFL Diploma + Research paper "Gaming in the classroom"</li>
                         <li>CTBE - Certificate for Teaching business English</li>
                         <li>TEFL</li>
-                        <li>4 A levels</li>
-                        <li>Guitar Grade 1-8 and Teaching Certificate</li>
-                        <li>UK Gambling Commission - PFL</li>
+                        <li>A levels</li>
                     </ul>
                 </section>	
             </div>
